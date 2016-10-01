0ffcb52d9a059109dca0bd1e812b0da65cc7df38
Sher Chowdhury <sherc1980@yahoo.com>
2016-10-01 17:14:21 +0100

    wip

--- Vagrantfile
+++ Vagrantfile
@@ -28,14 +28,14 @@ Vagrant.configure(2) do |config|
     puppet4master_config.vm.hostname = "puppetmaster.local"
 
 
-    # This will appear when you do "ip addr show". You can then access your guest machine's website using "http://192.168.50.4"
-    puppet4master_config.vm.network "private_network", ip: "192.168.51.100"
+    # This will appear when you do "ip addr show". You can then access your guest machine's website using "http://192.168.60.4"
+    puppet4master_config.vm.network "private_network", ip: "192.168.60.100"
     # note: this approach assigns a reserved internal ip addresses, which virtualbox's builtin router then reroutes the traffic to,
     #see: https://en.wikipedia.org/wiki/Private_network
 
     puppet4master_config.vm.provider "virtualbox" do |vb|
       # Display the VirtualBox GUI when booting the machine
-      vb.gui = true
+      vb.gui = false
       # For common vm settings, e.g. setting ram and cpu we use:
       vb.memory = "2048"
       vb.cpus = 2
@@ -45,9 +45,9 @@ Vagrant.configure(2) do |config|
       vb.name = "puppet4master"
     end
 
-    puppet4master_config.vm.provision :host_shell do |host_shell|
-      host_shell.inline = "cp -f ${HOME}/.gitconfig ./personal-data/.gitconfig"
-    end
+    config.vm.provision "file", source: "~/.gitconfig", destination: ".gitconfig"
+    config.vm.provision "file", source: "files/pe.conf", destination: "/pe.conf"
+
 
     puppet4master_config.vm.provision "shell" do |s|
       s.inline = '[ -f /vagrant/personal-data/.gitconfig ] && runuser -l vagrant -c "cp -f /vagrant/personal-data/.gitconfig ~"'
@@ -63,9 +63,9 @@ Vagrant.configure(2) do |config|
     puppet4master_config.vm.provision "shell", path: "scripts/update-git.sh"
     puppet4master_config.vm.provision "shell", path: "scripts/install-vim-puppet-plugins.sh", privileged: false
     # for some reason I have to restart network, but this needs more investigation
-    puppet4master_config.vm.provision "shell" do |remote_shell|
-      remote_shell.inline = "systemctl restart network"
-    end
+#    puppet4master_config.vm.provision "shell" do |remote_shell|
+#      remote_shell.inline = "systemctl restart network"
+#    end
 
     # this takes a vm snapshot (which we have called "basline") as the last step of "vagrant up".
     puppet4master_config.vm.provision :host_shell do |host_shell|
@@ -81,7 +81,7 @@ Vagrant.configure(2) do |config|
     config.vm.define "puppet4agent0#{i}" do |puppet4agent_config|
       puppet4agent_config.vm.box = "centos/7"
       puppet4agent_config.vm.hostname = "puppetagent0#{i}.local"
-      puppet4agent_config.vm.network "private_network", ip: "192.168.51.10#{i}"
+      puppet4agent_config.vm.network "private_network", ip: "192.168.60.10#{i}"
       puppet4agent_config.vm.provider "virtualbox" do |vb|
         vb.gui = false
         vb.memory = "1024"
@@ -109,20 +109,20 @@ Vagrant.configure(2) do |config|
   # it adds entry to the /etc/hosts file.
   # this block is placed outside the define blocks so that it gts applied to all VMs that are defined in this vagrantfile.
   config.vm.provision :hosts do |provisioner|
-    provisioner.add_host '192.168.51.100', ['puppetmaster', 'puppetmaster.local']
-    provisioner.add_host '192.168.51.101', ['puppetagent01', 'puppetagent01.local']
-    provisioner.add_host '192.168.51.102', ['puppetagent02', 'puppetagent02.local']
+    provisioner.add_host '192.168.60.100', ['puppetmaster', 'puppetmaster.local']
+    provisioner.add_host '192.168.60.101', ['puppetagent01', 'puppetagent01.local']
+    provisioner.add_host '192.168.60.102', ['puppetagent02', 'puppetagent02.local']
   end
 
   config.vm.provision :host_shell do |host_shell|
-    host_shell.inline = 'hostfile=/c/Windows/System32/drivers/etc/hosts && grep -q 192.168.51.100 $hostfile || echo "192.168.50.100   puppet4master puppet4master.local" >> $hostfile'
+    host_shell.inline = 'hostfile=/c/Windows/System32/drivers/etc/hosts && grep -q 192.168.60.100 $hostfile || echo "192.168.60.100   puppet4master puppet4master.local" >> $hostfile'
   end
 
   config.vm.provision :host_shell do |host_shell|
-    host_shell.inline = 'hostfile=/c/Windows/System32/drivers/etc/hosts && grep -q 192.168.51.101 $hostfile || echo "192.168.50.101   puppet4agent01 puppet4agent01.local" >> $hostfile'
+    host_shell.inline = 'hostfile=/c/Windows/System32/drivers/etc/hosts && grep -q 192.168.60.101 $hostfile || echo "192.168.60.101   puppet4agent01 puppet4agent01.local" >> $hostfile'
   end
 
   config.vm.provision :host_shell do |host_shell|
-    host_shell.inline = 'hostfile=/c/Windows/System32/drivers/etc/hosts && grep -q 192.168.51.102 $hostfile || echo "192.168.50.102   puppet4agent02 puppet4agent02.local" >> $hostfile'
+    host_shell.inline = 'hostfile=/c/Windows/System32/drivers/etc/hosts && grep -q 192.168.60.102 $hostfile || echo "192.168.60.102   puppet4agent02 puppet4agent02.local" >> $hostfile'
   end
 end
--- scripts/install-puppet4master.sh
+++ scripts/install-puppet4master.sh
@@ -5,63 +5,80 @@ echo '##### About to run install-puppetmaster4.sh script #######################
 echo '##########################################################################'
 
 
-yum install -y ruby-devel   # this line is no longer required, so delete it since it has been added in the kickstart file.
-
-# this is really important:
-# http://docs.puppetlabs.com/puppet/4.3/reference/whered_it_go.html#new-codedir-holds-all-modulesmanifestsdata
-
-
-# This shows the location of all the various puppet config files. 
-# https://github.com/puppetlabs/puppet-specifications/blob/master/file_paths.md#puppetserver
-
-# puppet.conf is now stored in /etc/puppetlabs/puppet/puppet.conf   
-# This is specified here:
-# http://docs.puppetlabs.com/puppet/4.3/reference/whered_it_go.html#nix-confdir-is-now-etcpuppetlabspuppet
-
-# The environments folder (/etc/puppet/environment) is now located at: 
-# http://docs.puppetlabs.com/puppet/4.3/reference/whered_it_go.html#new-codedir-holds-all-modulesmanifestsdata
+yum install -y ruby-devel
+yum install -y wget
+yum install -y epel-release
+yum install -y jq
+yum install -y vim
+yum install -y htop
+yum install -y tree
 
 
-rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm  || exit 1
+cd /root
 
-yum install -y puppetserver || exit 1
+wget -O - https://downloads.puppetlabs.com/puppetlabs-gpg-signing-key.pub | gpg --import
+curl -L -o pe-latest.tgz 'https://pm.puppetlabs.com/cgi-bin/download.cgi?dist=el&rel=7&arch=x86_64&ver=latest'
+
+tar -xf pe-latest.tgz
+
+wget https://gist.githubusercontent.com/Sher-Chowdhury/d402b0a1267cc6ae767b86b710ec4fe3/raw/86b0a4a78be833dc4eaf1a4c77fa4994b07945ef/pe.conf
+
+cd /root/puppet-enterprise*
+
+
+
+./puppet-enterprise-installer -c /root/pe.conf  > /root/pe-install-result.log
+
+/opt/puppetlabs/bin/puppet agent -t
+/opt/puppetlabs/bin/puppet agent -t
+
+/opt/puppetlabs/bin/puppet module install puppet-zabbix
+
+/opt/puppetlabs/bin/puppet agent -t
+
+#rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm  || exit 1
+
+#yum install -y puppetserver || exit 1
 
 
 # https://docs.puppetlabs.com/puppetserver/latest/install_from_packages.html#memory-allocation
-sed -i s/^JAVA_ARGS/#JAVA_ARGS/g /etc/sysconfig/puppetserver || exit 1
+#sed -i s/^JAVA_ARGS/#JAVA_ARGS/g /etc/sysconfig/puppetserver || exit 1
 #echo 'JAVA_ARGS="-Xms512m -Xmx512m -XX:MaxPermSize=256m"' >> /etc/sysconfig/puppetserver
-echo 'JAVA_ARGS="-Xms512m -Xmx512m"' >> /etc/sysconfig/puppetserver || exit 1
+#echo 'JAVA_ARGS="-Xms512m -Xmx512m"' >> /etc/sysconfig/puppetserver || exit 1
 
 
 # http://docs.puppetlabs.com/puppet/4.3/reference/whered_it_go.html
-echo "PATH=$PATH:/opt/puppetlabs/bin" >> /root/.bashrc || exit 1
-PATH=$PATH:/opt/puppetlabs/bin || exit 1
-puppet --version || exit 1
+#echo "PATH=$PATH:/opt/puppetlabs/bin" >> /root/.bashrc || exit 1
+#PATH=$PATH:/opt/puppetlabs/bin || exit 1
+#puppet --version || exit 1
 
-# this is so to get the puppetmaster to autosign puppet agent certificicates. 
+
+
+
+# this is so to get the puppetmaster to autosign puppet agent certificicates.
 # This means that you no longer need to do "puppet cert sign...etc"
 # https://docs.puppetlabs.com/puppet/latest/reference/ssl_autosign.html#basic-autosigning-autosignconf
 # https://docs.puppetlabs.com/puppet/latest/reference/config_file_autosign.html
 
-puppet config set autosign true --section master
+#puppet config set autosign true --section master
 # echo '*' >> /etc/puppet/autosign.conf   # this line needs fixing, see:
-					  # https://docs.puppetlabs.com/puppet/latest/reference/config_file_autosign.html 
+					  # https://docs.puppetlabs.com/puppet/latest/reference/config_file_autosign.html
 
 
 ##
-## The following is a direct copy taken from puppet 3.8 config file. So need to investigate which parts I need. 
+## The following is a direct copy taken from puppet 3.8 config file. So need to investigate which parts I need.
 ##
 ## https://docs.puppetlabs.com/puppet/latest/reference/config_about_settings.html
 ## https://docs.puppetlabs.com/puppet/latest/reference/configuration.html
-fqdn=`hostnamectl | grep 'Static hostname' | cut -d':' -f2`
+#fqdn=`hostnamectl | grep 'Static hostname' | cut -d':' -f2`
 
-puppet config set certname $fqdn --section agent 
-puppet config set server $fqdn --section agent 
+#puppet config set certname $fqdn --section agent
+#puppet config set server $fqdn --section agent
 
 #echo '    certname          = puppet4master.local' >> /etc/puppetlabs/puppet/puppet.conf
 #echo '    server            = puppet4master.local' >> /etc/puppetlabs/puppet/puppet.conf
 
-systemctl enable puppetserver || exit 1
-systemctl start puppetserver  || exit 1
-systemctl restart puppetserver  || exit 1
-systemctl status puppetserver || exit 1
+#systemctl enable puppetserver || exit 1
+#systemctl start puppetserver  || exit 1
+#systemctl restart puppetserver  || exit 1
+#systemctl status puppetserver || exit 1
